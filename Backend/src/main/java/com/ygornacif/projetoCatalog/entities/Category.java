1b5bbaa33542609894b0fd90474cf840821c7a11
Ygor Nacif <ygor.nacif00@gmail.com>
2024-10-12 16:50:05 -0300

    Category

---
+++ Backend/src/main/java/com/ygornacif/projetoCatalog/entities/Category.java
@@ -0,0 +1,55 @@
+package com.ygornacif.projetoCatalog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private long id;
+    private String name;
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ Backend/src/main/java/com/ygornacif/projetoCatalog/entities/Product.java
@@ -0,0 +1,84 @@
+package com.ygornacif.projetoCatalog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private String imgUrl;
+
+    public Product(long id, String name, String description, double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Product() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
