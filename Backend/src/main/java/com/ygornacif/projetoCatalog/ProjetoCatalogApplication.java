889ad36cc6c38441f48f386350e325d7997ef8af
Ygor Nacif <ygor.nacif00@gmail.com>
2024-10-12 10:34:00 -0300

    Create Project

---
+++ Backend/src/main/java/com/ygornacif/projetoCatalog/ProjetoCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.ygornacif.projetoCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCatalogApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ygornacif/projetoCatalog/ProjetoCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygornacif.projetoCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
