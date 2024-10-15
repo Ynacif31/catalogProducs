8176a9eb05ff14c9e999f7acfac7e378830515d6
Ygor Nacif <ygor.nacif00@gmail.com>
2024-10-15 17:11:21 -0300

    Created CategoryDTO

--- Backend/src/main/java/com/ygornacif/projetoCatalog/resources/CategoryResource.java
+++ Backend/src/main/java/com/ygornacif/projetoCatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.ygornacif.projetoCatalog.resources;
 
+import com.ygornacif.projetoCatalog.DTO.CategoryDTO;
 import com.ygornacif.projetoCatalog.entities.Category;
 import com.ygornacif.projetoCatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-       List<Category> list = categoryService.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+       List<CategoryDTO> list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- Backend/src/main/java/com/ygornacif/projetoCatalog/services/CategoryService.java
+++ Backend/src/main/java/com/ygornacif/projetoCatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.ygornacif.projetoCatalog.services;
 
+import com.ygornacif.projetoCatalog.DTO.CategoryDTO;
 import com.ygornacif.projetoCatalog.entities.Category;
 import com.ygornacif.projetoCatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -16,7 +18,8 @@ public class CategoryService {
 
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-      return categoryRepository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 }
