d94aa392f596219e1ce0c4f6c4ce09966fe1d554
Ygor Nacif <ygor.nacif00@gmail.com>
2024-10-24 16:37:42 -0300

    paginacao

--- Backend/src/main/java/com/ygornacif/projetoCatalog/resources/CategoryResource.java
+++ Backend/src/main/java/com/ygornacif/projetoCatalog/resources/CategoryResource.java
@@ -4,6 +4,9 @@ import com.ygornacif.projetoCatalog.DTO.CategoryDTO;
 import com.ygornacif.projetoCatalog.entities.Category;
 import com.ygornacif.projetoCatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -21,8 +24,13 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-       List<CategoryDTO> list = categoryService.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
